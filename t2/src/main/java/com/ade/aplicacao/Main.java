4e0afb81ce3d427e3a6c6c8f73b53c80482585ff
Artur Kalil <arturkcorr3a@gmail.com>
2024-11-25 18:29:50 -0300

    arquivos adicionados na estrutura Maven para permitir testes

---
+++ t2/src/main/java/com/ade/aplicacao/Main.java
@@ -0,0 +1,15 @@
+package com.ade.aplicacao;
+
+public class Main {
+    public static void main(String[] args) {
+        App app = new App();
+
+        Controle controle = new Controle();
+        System.out.println(controle.inicializaDados());
+        System.out.println(controle.salvaDados());
+
+        app.run();
+
+    }
+
+}
---
+++ t2/src/main/java/com/ade/cargas/TipoCarga.java
@@ -0,0 +1,28 @@
+package com.ade.cargas;
+
+public abstract class TipoCarga {
+
+	protected String descricao;
+	protected final double fatorPeso;
+	protected final int numero;
+
+	protected TipoCarga(String descricao, double fatorPeso, int numero) {
+		this.descricao = descricao;
+		this.fatorPeso = fatorPeso;
+		this.numero = numero;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	@Override
+	public String toString() {
+		return "Numero: " + numero + "\tDescricao: " + descricao + "\tFator de Peso: " + fatorPeso;
+	}
+
+	public String csvString() {
+		return fatorPeso + ";" + descricao + ";" + numero;
+	}
+
+}
